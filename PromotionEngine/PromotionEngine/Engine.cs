c9bb02cd938555b1eeed47f31dd6b044296e1634
Prabhu <prabhu.developer2010@gmail.com>
2021-07-09 07:46:18 +0530

    Added Engine class and init the constructor with promotion and skus. Also create the method to return the totalAmount of the order.

---
+++ PromotionEngine/PromotionEngine/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using PromotionEngine.Models;
+using System.Linq;
+
+namespace PromotionEngine
+{
+    public class Engine
+    {
+        List<Cart> _skus;
+        List<Promotion> _promotins;
+        public Engine(List<Cart> skus, List<Promotion> promotins)
+        {
+            _skus = skus;
+            _promotins = promotins;
+        }
+
+        public decimal CalculateTotalOrderValue()
+        {
+            if (_skus == null || _skus?.Count == 0)
+                return 0M;
+
+            if (_promotins == null || _promotins?.Count == 0)
+                return _skus.Sum(s => s.Quanity * s.sku.Price);
+
+            else
+                return 0M;
+        }
+    }
+}
